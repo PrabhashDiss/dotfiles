4e01e166fa6083622b44812efcc1cd234bbe4482
Dissanayake H.D.M.P.P. <prabhashdissanayake2k@gmail.com>
2025-11-14 09:33:13 +0530

    feat(fish): add vi key bindings and starship initialization (#10)
    * Initialize Starship prompt in Fish shell for enhanced terminal experience
    
    * Enable vi key bindings in fish shell for improved navigation and editing

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,16 @@ end
 function fish_prompt
     printf '\n%s\n› ' (pwd)
 end
+
+function fish_user_key_bindings
+    # Execute this once per mode that emacs bindings should be used in
+    fish_default_key_bindings -M insert
+
+    # Then execute the vi-bindings so they take precedence when there's a conflict.
+    # Without --no-erase fish_vi_key_bindings will default to
+    # resetting all bindings.
+    # The argument specifies the initial mode (insert, "default" or visual).
+    fish_vi_key_bindings --no-erase insert
+end
+
+starship init fish | source
